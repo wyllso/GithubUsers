363b1907b603f164ca599291d8eb3caa3f0f45f8
user <andikaponpes09@gmail.com>
2022-11-20 20:01:21 +0800

    Initial commit

---
+++ app/src/main/java/com/F55121061_Andhika/githubusers/User.java
@@ -0,0 +1,27 @@
+package com.F55121061_Andhika.githubusers;
+
+public class User {
+    private String potho;
+    private String name;
+    private String type;
+
+    public String getType(){
+        return type;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getPotho(){
+        return potho;
+    }
+    public void setPotho(String potho){
+        this.potho = potho;
+    }
+    public  void  setName (String name){
+        this.name = name;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
+
+}
